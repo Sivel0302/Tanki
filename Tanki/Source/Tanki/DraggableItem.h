d181459c098ab51d4e36c8db6912820e83d3d1d3
Sivel0302 <nikolai0302@yandex.ru>
2022-11-10 17:12:44 +0300

    create DraggableItem and FruitDragDropOperation classes
    создал класс для работы с драг и дроп и класс для передачи имени предмета который переносим

---
+++ Tanki/Source/Tanki/DraggableItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DraggableItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKI_API UDraggableItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Tanki/Source/Tanki/FruitDragDropOperation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/DragDropOperation.h"
+#include "FruitDragDropOperation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKI_API UFruitDragDropOperation : public UDragDropOperation
+{
+	GENERATED_BODY()
+	
+};
