b81fd3bcb6cc74ca59bacb843d0b11ca6202464a
Николай <104088789+Sivel0302@users.noreply.github.com>
2022-11-13 15:34:25 +0300

    create UIPlayerController class
    создан класс для организации драг и дроп в мир

---
+++ Tanki/Source/Tanki/GeekBrainsUIPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GeekBrainsUIPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKI_API AGeekBrainsUIPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
