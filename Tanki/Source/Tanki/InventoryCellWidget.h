7bee283f1aafc1253de24285adfc048183d2fa27
Николай <104088789+Sivel0302@users.noreply.github.com>
2022-11-17 19:02:09 +0300

    создал блюпринты и класс инвентаря

--- Tanki/Source/Tanki/InventoryCellWidget.h
+++ Tanki/Source/Tanki/InventoryCellWidget.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InventoryItem.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
 #include "InventoryCellWidget.generated.h"
 
 /**
@@ -13,5 +16,21 @@ UCLASS()
 class TANKI_API UInventoryCellWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+    bool HasItem() { return bHasItem; }
+    bool AddItem(const FInventorySlotInfo& Item, const FInventoryItemInfo& ItemInfo);
+    void Clear();
+    const FInventorySlotInfo& GetItem();
+    int32 IndexInInventory = -1;
+protected:
+    bool bHasItem;
+    UPROPERTY(meta = (BindWidgetOptional))
+    UImage* ItemImage;
+    UPROPERTY(meta = (BindWidgetOptional))
+    UTextBlock* CountText;
+    UPROPERTY()
+    FInventorySlotInfo StoredItem;
+
 	
 };
