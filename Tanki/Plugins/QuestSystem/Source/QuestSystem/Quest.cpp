8afecaeaeb654e98c72f949c338322bbf6d54df7
Николай <nikolai0302@yandex.ru>
2022-12-05 19:19:26 +0300

    исправил баг с полем квеста и создал Quest class
    в классе закомментаировал часть кода которая не компилируется

---
+++ Tanki/Plugins/QuestSystem/Source/QuestSystem/Quest.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Quest.h"
+
+
+// Sets default values
+AQuest::AQuest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AQuest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AQuest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AQuest::UpdateLocation()
+{
+	AActor * ParentActor = GetAttachParentActor();
+	if (ParentActor)
+	{
+		SetActorLocation(ParentActor->GetActorLocation());
+	}
+}
+
---
+++ Tanki/Plugins/QuestSystem/Source/QuestSystem/Quest.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Quest.generated.h"
+
+UCLASS()
+class QUESTSYSTEM_API AQuest : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AQuest();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	FText Name;
+	UPROPERTY(EditAnywhere)
+	FText Descrition;
+	/*UPROPERTY(EditAnywhere)
+	TArray<class UObjective*> Objectives;*/
+	UPROPERTY(EditAnywhere)
+	bool bIsStoryQuest = true;
+	UPROPERTY(EditAnywhere)
+	bool bKeepObjectivesOrder = true;
+	UPROPERTY(EditAnywhere)
+	AQuest* PrerquisedQuest;
+	UPROPERTY(VisibleAnywhere)
+	bool bIsTaken;
+
+
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void UpdateLocation();
+	
+};
