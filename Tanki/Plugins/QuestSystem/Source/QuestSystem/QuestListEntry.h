6e90525a27a8d6599bab390dca47c7d2e2a9b388
Николай <nikolai0302@yandex.ru>
2022-12-09 10:37:54 +0300

    пополнил класс списка квестов

--- Tanki/Plugins/QuestSystem/Source/QuestSystem/QuestListEntry.h
+++ Tanki/Plugins/QuestSystem/Source/QuestSystem/QuestListEntry.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Border.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "QuestListEntry.generated.h"
 
 /**
@@ -13,5 +16,20 @@ UCLASS()
 class QUESTSYSTEM_API UQuestListEntry : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void NativeConstruct() override;
+	void SetQuestText(const FText& Text);
+	void SetIsSelected(bool IsSelected);
+	FSimpleDelegate OnQuestChosen;
+protected:
+	UPROPERTY(meta = (BindWidgetOptional))
+	UTextBlock * QuestText;
+	UPROPERTY(meta = (BindWidgetOptional))
+	UButton * SelectQuestButton;
+	UPROPERTY(meta = (BindWidgetOptional))
+	UBorder * SelectionBorder;
+	UFUNCTION()
+	void SelectQuest();
 	
 };
