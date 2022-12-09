ae2994c14e16425427a0be8f8c1390d8c95b487b
Николай <nikolai0302@yandex.ru>
2022-12-09 07:21:07 +0300

    создал класс виджета квест диалога + виджет текущих квестов от класса

---
+++ Tanki/Plugins/QuestSystem/Source/QuestSystem/QuestDialog.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QuestDialog.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestDialog : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
